51c35354d45ed6d411c235e2e9549d882e52d908
onofreid <dorin.onofrei@moodys.com>
2024-01-06 12:07:46 -0500

    Adding full asset validation

--- cypress/cypress/e2e/endpoints-from-crawler.bei.ts
+++ cypress/cypress/e2e/endpoints-from-crawler.bei.ts
@@ -2,40 +2,89 @@ import { CrawledData } from '../fixtures/crawledEndpoints'
 
 describe('Endpoint Health Checks', () => {
   const endpoints = CrawledData.endpoints
-  // Explicitly type the array to store strings
-  const failedEndpoints: string[] = []
+  const failedAssets: string[] = []
+  const failedPages: string[] = []
+  const fileName = 'cypress/downloads/failedAssetsCrawler.txt'
+  const pageFileName = 'cypress/downloads/failedPagesCrawler.txt'
 
   before(() => {
-    cy.writeFile('cypress/downloads/failedEndpoints.txt', '', { flag: 'w' })
+    cy.writeFile(fileName, '', { flag: 'w' })
+    cy.writeFile(pageFileName, '', { flag: 'w' })
   })
 
   endpoints.forEach((url) => {
-    it(`Validating - ${url}`, () => {
-      cy.request({
-        url: url,
-        failOnStatusCode: false,
-      }).then((response) => {
-        if (response.status !== 200) {
-          failedEndpoints.push(
-            `Failed Endpoint: ${url}, Status: ${response.status}`
-          )
-        }
+    it(`Validating page and assets for - ${url}`, () => {
+      // Check if the URL is likely a non-HTML resource
+      if (url.endsWith('.pdf')) {
+        // Directly use cy.request for non-HTML resources
+        cy.request({ url: url, failOnStatusCode: false }).then((response) => {
+          if (response.status !== 200) {
+            failedPages.push(
+              `Failed Resource: ${url}, Status: ${response.status}`
+            )
+          }
+        })
+      } else {
+        // Proceed with normal HTML page checks
+        cy.request({ url: url, failOnStatusCode: false }).then(
+          (pageResponse) => {
+            if (pageResponse.status !== 200) {
+              failedPages.push(
+                `Failed Page: ${url}, Status: ${pageResponse.status}`
+              )
+            } else {
+              cy.visit(url)
+              cy.document()
+                .then((document) => {
+                  const assets = Array.from(
+                    document.querySelectorAll(
+                      'link[rel="stylesheet"], script[src]'
+                    )
+                  )
+                    .map((el) =>
+                      el.tagName === 'LINK'
+                        ? (el as HTMLLinkElement).href
+                        : (el as HTMLScriptElement).src
+                    )
+                    .filter(Boolean)
 
-        expect(response.status).to.eq(200)
-      })
+                  // Use Cypress.Promise.all to wait for all requests to complete
+                  return Cypress.Promise.all(
+                    assets.map((asset) => {
+                      return cy
+                        .request({ url: asset, failOnStatusCode: false })
+                        .then((response) => {
+                          if (response.status !== 200) {
+                            failedAssets.push(
+                              `Failed Asset: ${asset} from ${url}, Status: ${response.status}`
+                            )
+                          }
+                        })
+                    })
+                  )
+                })
+                .then(() => {
+                  // Assert at the end of the test
+                  expect(failedAssets.length, `Failed assets for ${url}`).to.eq(
+                    0
+                  )
+                })
+            }
+          }
+        )
+      }
     })
   })
 
   after(() => {
-    if (failedEndpoints.length > 0) {
-      const logContent = failedEndpoints.join('\n')
-      cy.writeFile(
-        'cypress/downloads/postCrawl_failedEndpoints.txt',
-        logContent,
-        {
-          flag: 'a+',
-        }
-      )
+    // Write to files in the after hook
+    if (failedPages.length > 0) {
+      const pageLogContent = failedPages.join('\n')
+      cy.writeFile(pageFileName, pageLogContent, { flag: 'a+' })
+    }
+    if (failedAssets.length > 0) {
+      const assetLogContent = failedAssets.join('\n')
+      cy.writeFile(fileName, assetLogContent, { flag: 'a+' })
     }
   })
 })
